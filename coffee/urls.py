01e11b1af24be8faf18a26033d5ae14fb667f682
pepebotika69 <anti_todo@msn.com>
2022-11-30 09:43:10 +0300

    Do some changes for vercel

--- coffee/urls.py
+++ coffee/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from coffee.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('coffee/', include('coffee.urls')),
+    path('', index),
 ]
--- coffee/views.py
+++ coffee/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
 # Create your views here.
+from datetime import datetime
+
+from django.http import HttpResponse
+
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello!</h1>
+            <p>The current time is {now}.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
