2c016051dca96bfb48ed08584e718328fe59aee2
pepebotika69 <anti_todo@msn.com>
2022-12-03 21:03:02 +0300

    Add some comments to logal entity model

--- coffee/models/legal_entity.py
+++ coffee/models/legal_entity.py
@@ -5,7 +5,10 @@ from model_utils.models import TimeStampedModel
 
 class LegalEntity(TimeStampedModel):
     name = models.CharField(verbose_name=_('Nombre'), unique=True, max_length=255)
+
+    # TODO do I need to add related_name
     parent = models.ForeignKey('self', verbose_name=_('Padre'), blank=True, null=True, on_delete=models.RESTRICT)
+
     is_active = models.BooleanField(verbose_name=_('Activo'), default=True)
 
     class Meta:
