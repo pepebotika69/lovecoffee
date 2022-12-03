c1088dc838cb6cf029e5cf79d0bd9f7a73f21949
pepebotika69 <anti_todo@msn.com>
2022-12-03 23:03:38 +0300

    Add model coffee address

--- coffee/models/__init__.py
+++ coffee/models/__init__.py
@@ -1,2 +1,3 @@
 from coffee.models import legal_entity
 from coffee.models import brand
+from coffee.models import coffee_address
---
+++ coffee/models/coffee_address.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
+
+from coffee.models.brand import Brand
+from common.models.city import City
+
+
+class CoffeeAddress(TimeStampedModel):
+    city = models.ForeignKey(City, verbose_name=_('Localidad'), related_name='city', on_delete=models.RESTRICT)
+
+    # TODO should I divide address in to fields
+    address = models.TextField(verbose_name=_('Dirección'))
+
+    brand = models.ForeignKey(Brand, verbose_name=_('Marca'), on_delete=models.RESTRICT, related_name='brand')
+
+    is_active = models.BooleanField(verbose_name=_('Activo'), default=True)
+
+    class Meta:
+        verbose_name = _('Dirección')
+        verbose_name_plural = _('Direcciones')
+        constraints = [
+            models.UniqueConstraint(
+                name='unique_coffee_coffee_address_city_address_brand',
+                fields=['city', 'address', 'brand']
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.address}({self.pk})'
