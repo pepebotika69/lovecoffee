58defb6e4b5d04122a9a48e639827669b68a667f
pepebotika69 <anti_todo@msn.com>
2022-12-03 18:05:46 +0300

    change settings

--- lovecoffee/settings.py
+++ lovecoffee/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'lovecoffee.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    #"default": dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=1800),
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=1800),
 }
 
 # DATABASES = {
