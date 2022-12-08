b8a690c79d5d0a2a0ed61fa3be5d6cadd2293a10
pepebotika69 <anti_todo@msn.com>
2022-12-08 17:01:32 +0300

    Add judge application

---
+++ judge/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from judge.models.judge import Judge
+
+admin.site.register(Judge)
---
+++ judge/models/judge.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Judge(models.Model):
+    user = models.OneToOneField(User, verbose_name=_('Usuario'), related_name='user', on_delete=models.RESTRICT)
+
+    # TODO suares maybe use float
+    reputation = models.IntegerField(
+        verbose_name=_('Reputación'),
+        null=True,
+        blank=True,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
+    )
+
+    can_comment = models.BooleanField(verbose_name=_('Puede comentar?'), default=False)
+
+    can_rank = models.BooleanField(verbose_name=_('Puede calificar?'), default=False)
+
+    is_active = models.BooleanField(verbose_name=_('Activo'), default=False)
